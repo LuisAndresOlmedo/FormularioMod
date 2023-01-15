0b79ec2866f0a908f85c4ef411dbab018f4da0fd
Luis <olmedoluisandres@gmail.com>
2023-01-15 06:23:13 -0300

    Avances con la tabla de Usuarios

--- Filtro o Juego.js
+++ Filtro o Juego.js
@@ -3,7 +3,7 @@ const button = document.getElementById("Guardando");
 const codigoSeg = document.getElementById("Seguridad:");
 const etiquetaPos = document.getElementById("Cliente");
 const Tocador = document.getElementById("OnclickVacio");
-var imagenes = document.getElementById('ImagenPerfil');
+var imagenes = document.getElementById("ImagenPerfil");
 var Consutas = document.getElementById("Consultas");
 var Apellido = document.getElementById("Apellido");
 var Inputinit = document.getElementById("Nombres");
@@ -13,42 +13,69 @@ var mes = document.getElementById("fechaNacimiento");
 var Correito = document.getElementById("Correo");
 let EtiquetaP = {};
 var Etiquetas = "Se cargaran los datos";
+imagenes.src = "./I/Usuario.png";
+document.querySelector(".error").style.visibility = "visible";
 
 let Usuario = [
   {
     nombres: "Luis Andres",
     apellido: "Olmedo",
     consultas: "¿Reparaciones?",
+    Imagen: "./I/Usuario.png",
+    Mes: "2022-07",
+    Pass: "1234567",
+    dia: "12",
+    email: "olmedoluisandres@gmial.com",
     id: 8000,
-    Identificador: 0
+    Identificador: 0,
   },
   {
     nombres: "Juan",
-    apellido: "Andres",
+    apellido: "Carlos",
     consultas: "¿Reparaciones?",
+    Imagen: "./I/Usuario.png",
+    Mes: "2022-07",
+    Pass: "1234567",
+    dia: "12",
+    email: "juancarlos@gmial.com",
     id: 36,
-    Identificador: 1
+    Identificador: 1,
   },
   {
     nombres: "Jose",
     apellido: "Montenegro",
     consultas: "¿Reparaciones?",
+    Imagen: "./I/Usuario.png",
+    Mes: "2022-07",
+    Pass: "1234567",
+    dia: "12",
+    email: "josemontenegro@gmial.com",
     id: 15,
-    Identificador: 2
+    Identificador: 2,
   },
   {
     nombres: "Leo",
     apellido: "Villalva",
     consultas: "¿Reparaciones?",
-    id: "10",
-    Identificador: 3
+    Imagen: ".I/Usuario.png",
+    Mes: "2022-07",
+    Pass: "1234567",
+    dia: "12",
+    email: "leonelvillalva@gmial.com",
+    id: 10,
+    Identificador: 3,
   },
   {
     nombres: "Fernando",
     apellido: "Flores",
     consultas: "¿Reparaciones?",
-    id: "9",
-    Identificador: 4
+    Imagen: "./I/Usuario.png",
+    Mes: "2022-07",
+    Pass: "1234567",
+    dia: "12",
+    email: "fernandoflores@gmial.com",
+    id: 9,
+    Identificador: 4,
   },
 ];
 
@@ -59,31 +86,31 @@ etiquetaPos.innerHTML = "<H1>Informacón personal:</H1>" + Etiquetas; //Iniciali
 function azares() {
   var numeroUsuario = cantidadUsuario;
   var azar = Math.random() * 1000000000;
-  var redondeo = Math.floor(azar)
+  var redondeo = Math.floor(azar);
   let numeros = numeroUsuario * redondeo;
-  return numeros
+  return numeros;
 }
 
-button.addEventListener('click', () => {
-  const exportarid = "<p>"+"Codigo se Seguridad:" +Usuario[Usuario.length - 1].id+"</p>";
+button.addEventListener("click", () => {
+  const exportarid =
+    "<p>" + "Codigo se Seguridad:" + Usuario[Usuario.length - 1].id + "</p>";
   codigoSeg.innerHTML = exportarid;
 });
 
 //---------------------------------------------------------------------------------Buscador
 
-
 function Buscar() {
-  var Personas = Usuario.find(Encontrado => Encontrado.id == valor.value);
-    if (Personas) {
-      pushDatos();
-      document.querySelector('.FormulariosidBusqueda').style.visibility = 'hidden';
-      document.querySelector('#Guardando').style.display = 'none';
-      document.querySelector('#Guardando').style.display = 'none';
-    }
-    else {
-      console.log("Usuario no encontrado");
-      document.querySelector('#error').style.background = '#ff520029';
-    }
+  var Personas = Usuario.find((Encontrado) => Encontrado.id == valor.value);
+  if (Personas) {
+    pushDatos();
+    document.querySelector(".FormulariosidBusqueda").style.visibility =
+      "hidden";
+    document.querySelector("#Guardando").style.display = "none";
+    document.querySelector("#Guardando").style.display = "none";
+  } else {
+    console.log("Usuario no encontrado");
+    document.querySelector("#error").style.background = "rgb(241 137 137)";
+  }
 
   function pushDatos() {
     Inputinit.value = Personas.nombres;
@@ -93,45 +120,45 @@ function Buscar() {
     mes.valor = Personas.Mes;
     imagenes.value = Personas.Imagen;
     dia.value = Personas.dia;
-    imagenes.value = Personas.Imagen
-  };
-};
-
+    imagenes.src = Personas.Imagen;
+  }
+  return Personas.Identificador;
+}
 
 function reEditarDatos() {
   var valores = Usuario.push({
-    nombres: Usuario[Constante].nombres = Usuario[Constante].nombres,
-    apellido: Buscar().apellido  = Usuario[Constante].apellido,
-    consultas: Buscar().consultas  = Usuario[Constante].consultas,
-    id: Buscar().id  = Usuario[Constante].id,
-    Imagen: Buscar().Imagen = Usuario[Constante].Imagen
-  })
-};
+    nombres: (Usuario[Constante].nombres = Usuario[Constante].nombres),
+    apellido: (Buscar().apellido = Usuario[Constante].apellido),
+    consultas: (Buscar().consultas = Usuario[Constante].consultas),
+    id: (Buscar().id = Usuario[Constante].id),
+    Imagen: (Buscar().Imagen = Usuario[Constante].Imagen),
+  });
+}
 
 //---------------------------------------------------------------------------------Buscador
 
 function DatosUsuarios() {
   soloNombre = Inputinit.value;
   soloApellido = Apellido.value;
-  soloImagen = imagenes.value;
+  soloImagen = imagenes.src;
   soloDia = dia.value;
   soloConsultas = Consutas.value;
   soloContra = Contraseña.value;
   soloMes = mes.value;
   soloEmail = Correito.value;
 
-    (soloNombre = Usuario.push({
-      nombres: soloNombre,
-      apellido: soloApellido,
-      Imagen: soloImagen,
-      email: soloEmail,
-      Pass: soloContra,
-      dia: soloDia,
-      Mes: soloMes,
-      consultas: soloConsultas,
-      Identificador: cantidadUsuario,
-      id: azares(),
-    }));
+  soloNombre = Usuario.push({
+    nombres: soloNombre,
+    apellido: soloApellido,
+    Imagen: soloImagen,
+    email: soloEmail,
+    Pass: soloContra,
+    dia: soloDia,
+    Mes: soloMes,
+    consultas: soloConsultas,
+    Identificador: cantidadUsuario,
+    id: azares(),
+  });
 
   ImprimirDatos();
 }
@@ -139,26 +166,36 @@ function DatosUsuarios() {
 //---------------------------------------------------------------------------------Pusheo de Datos
 
 function ImprimirDatos() {
-  const valorUsuario = Usuario.find(Encontrado => Encontrado.id == valor.value);
+  const valorUsuario = Usuario.find(
+    (Encontrado) => Encontrado.id == valor.value
+  );
   if (valorUsuario) {
     var Constante = valorUsuario.Identificador;
     Datos =
-    "<spam>Nombre:</spam>" + Usuario[Constante].nombres +
-    "<br/>" +
-    "<spam>Apellido:</spam>" + Usuario[Constante].apellido +
-    "<br/>" +
-    "<spam>Consultas:</spam>" + Usuario[Constante].consultas +
-    "<br/>" +
-    "<spam>Codigo Secreto:</spam>" + Usuario[Constante].id
+      "<spam>Nombre:</spam>" +
+      Usuario[Constante].nombres +
+      "<br/>" +
+      "<br/>" +
+      "<spam>Apellido:</spam>" +
+      Usuario[Constante].apellido +
+      "<br/>" +
+      "<br/>" +
+      "<spam>Consultas:</spam>" +
+      Usuario[Constante].consultas +
+      "<br/>" +
+      "<br/>" +
+      "<spam>Codigo Secreto:</spam>" +
+      Usuario[Constante].id;
     etiquetaPos.innerHTML = "<H1>Cargados:</H1>" + Datos;
   } else {
     etiquetaPos.innerHTML = "<spam>Se ha guardado tu usuario ✔</spam>";
   }
-  
 }
 
 function Renombrar() {
-  const valorUsuario = Usuario.find(Encontrado => Encontrado.id == valor.value);
+  const valorUsuario = Usuario.find(
+    (Encontrado) => Encontrado.id == valor.value
+  );
   var Constante = valorUsuario.Identificador;
   Usuario[Constante].nombres = Inputinit.value;
   Usuario[Constante].apellido = Apellido.value;
@@ -172,22 +209,87 @@ function Renombrar() {
 
 // Imprime el nombre de un arrays
 
-// Codigo para arrays
-
-var Prueba = ["¿Hace tu pregunta?",];
+var Prueba = ["¿Hace tu pregunta?"];
 
 function Actualizar() {
   const info = Tocador.value;
-  let numerossumados = Prueba.length; 
+  let numerossumados = Prueba.length;
   valoresfinaes = Prueba.push({
     mensaje: info,
-    N: Prueba.length
-  })
-    const ImprimirD = "<tr>" + Prueba[numerossumados].mensaje + "</tr>" + "<br/>";
+    N: Prueba.length,
+  });
+  const ImprimirD = "<tr>" + Prueba[numerossumados].mensaje + "</tr>" + "<br/>";
   document.getElementById("table").innerHTML = ImprimirD;
-};
+}
 
-document.getElementById("Actualizar").innerHTML = Prueba; 
+document.getElementById("Actualizar").innerHTML = Prueba;
 
-// imprime actualizacion
-//La idea hacer un chat bots pero tambien la consulta
+//La idea hacer un chat bots pero tambien la consulta, imprime actualizacion.
+
+var columna = document.getElementById("miTabla");
+var columnaId = 0;
+var columnaR = 0;
+
+function numerosMas() {
+  for (let i = 0; i <= Usuario.length - 1; i++) {
+    columnaR++;
+    console.log(columnaR);
+    tablas();
+    tablas2();
+    tablas3();
+    tablas4();
+    tablas5();
+    tablas6();
+    tablas7();
+  }
+  return columnaR
+}
+
+function tablas() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].nombres));
+  row.appendChild(tablita);
+}
+
+function tablas2() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].apellido));
+  row.appendChild(tablita);
+}
+
+function tablas3() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].consultas));
+  row.appendChild(tablita);
+}
+
+function tablas4() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].email));
+  row.appendChild(tablita);
+}
+
+function tablas5() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].Pass));
+  row.appendChild(tablita);
+}
+
+function tablas6() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].id));
+  row.appendChild(tablita);
+}
+
+function tablas7() {
+  var row = columna.rows[columnaR];
+  var tablita = document.createElement("td");
+  tablita.appendChild(document.createTextNode(Usuario[columnaR].Mes));
+  row.appendChild(tablita);
+}
